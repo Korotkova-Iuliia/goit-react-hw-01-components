e9ebf1f858c57c43316e13bdf8e68fa4e00d4c4e
Korotkova-Iuliia <3589900@gmail.com>
2021-12-29 03:00:52 +0200

    styles statistic

--- src/components/StatisticsList/StatisticList.jsx
+++ src/components/StatisticsList/StatisticList.jsx
@@ -1,11 +1,16 @@
 import styles from '../statistics/Statistics.module.css';
-
+import { getRandomHexColor } from '../helpers/RandomColor';
+console.log(getRandomHexColor());
 export const StatisticsList = ({ stats }) => {
   return (
     <ul className={styles.stat_list}>
       {stats.map(({ id, label, percentage }) => {
         return (
-          <li key={id} className={styles.item}>
+          <li
+            key={id}
+            className={styles.item}
+            style={{ backgroundColor: `${getRandomHexColor()} ` }}
+          >
             <span className={styles.label}>{label}</span>
             <span className={styles.percentage}>{percentage}%</span>
           </li>
